9834e1b0bd7e6e8966c8670a772f134556913b1a
Jack Williams <jackmwilliams35@gmail.com>
2023-08-10 15:47:09 -0500

    completed lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,10 +3,9 @@ require ( './root.js' );
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO');
-  })
+    expect(shout('hello'.toUpperCase())).toEqual('HELLO');
 })
-
+})
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello');
